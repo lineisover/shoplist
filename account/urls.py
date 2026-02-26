000968244f54c97dd5b745a10aabb833047bc3b8
владислав курочкин <lineisover@gmail.com>
2026-02-26 19:36:07 +0300

    fix path

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,6 @@ from .views import register
 app_name = 'account'
 
 urlpatterns = [
-    path('/<int:user_id>', views.profile, name='profile'),
+    path('<int:user_id>/', views.profile, name='profile'),
     path('register/', register, name='register'),
 ]
